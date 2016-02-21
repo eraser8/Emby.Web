8f2800eab85e62c283b170f45a73e2b48dd5899b
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-20 19:15:22 -0500

    rework startup scripts

--- startup/login.js
+++ startup/login.js
@@ -1,4 +1,119 @@
-define(['loading', 'connectionManager', 'startup/startuphelper'], function (loading, connectionManager, startupHelper) {
+define(['loading', 'connectionManager', './startuphelper', 'focusManager', 'coreIcons'], function (loading, connectionManager, startupHelper, focusManager) {
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function renderLoginUsers(view, apiClient, users, serverId, initScroller) {
+
+        var items = users.map(function (user) {
+
+            var imgUrl = user.PrimaryImageTag ?
+                apiClient.getUserImageUrl(user.Id, {
+                    width: 400,
+                    tag: user.PrimaryImageTag,
+                    type: "Primary"
+                }) :
+                '';
+
+            var url = user.HasPassword ?
+                ('/startup/manuallogin.html?serverid=' + serverId + '&user=' + user.Name) :
+                '';
+
+            return {
+                name: user.Name,
+                showIcon: !imgUrl,
+                showImage: imgUrl,
+                icon: 'person',
+                cardImageStyle: "background-image:url('" + imgUrl + "');",
+                id: user.Id,
+                url: url,
+                serverId: user.ServerId,
+                defaultText: true
+            };
+
+        });
+
+        items.push({
+            name: Globalize.translate('core#ButtonManualLogin'),
+            showIcon: true,
+            showImage: false,
+            icon: 'lock',
+            cardImageStyle: '',
+            cardType: 'manuallogin',
+            defaultText: true,
+            url: '/startup/manuallogin.html?serverid=' + serverId
+        });
+
+        items.push({
+            name: Globalize.translate('core#EmbyConnect'),
+            showIcon: true,
+            showImage: false,
+            icon: 'cloud',
+            cardImageStyle: '',
+            cardType: 'embyconnect',
+            defaultText: true,
+            url: '/startup/connectlogin.html'
+        });
+
+        items.push({
+            name: Globalize.translate('core#ButtonChangeServer'),
+            showIcon: true,
+            showImage: false,
+            icon: 'cast',
+            cardImageStyle: '',
+            cardType: 'changeserver',
+            defaultText: true,
+            url: '/startup/selectserver.html'
+        });
+
+        var html = items.map(function (item) {
+
+            var secondaryText = item.defaultText ? '&nbsp;' : '';
+
+            var cardImageContainer;
+
+            if (item.showIcon) {
+                cardImageContainer = '<iron-icon class="cardImageIcon" icon="' + item.icon + '"></iron-icon>';
+            } else {
+                cardImageContainer = '<div class="cardImage" style="' + item.cardImageStyle + '"></div>';
+            }
+
+            var tagName = 'paper-button';
+            var innerOpening = '<div class="cardBox">';
+            var innerClosing = '</div>';
+
+            return '\
+<' + tagName + ' raised class="card squareCard loginSquareCard scalableCard" data-cardtype="' + item.cardType + '" data-url="' + item.url + '" data-name="' + item.name + '" data-serverid="' + item.serverId + '">\
+'+ innerOpening + '<div class="cardScalable">\
+<div class="cardPadder"></div>\
+<div class="cardContent">\
+<div class="cardImageContainer coveredImage defaultCardColor'+ getRandomInt(1, 5) + '">\
+'+ cardImageContainer + '</div>\
+</div>\
+</div>\
+<div class="cardFooter">\
+<div class="cardText">'+ item.name + '</div>\
+<div class="cardText dim">' + secondaryText + '</div>\
+</div>'+ innerClosing + '\
+</'+ tagName + '>';
+
+        }).join('');
+
+        var scrollSlider = view.querySelector('.scrollSlider');
+        scrollSlider.innerHTML = html;
+
+        require(["Sly"], function (Sly) {
+
+            loading.hide();
+
+            if (initScroller) {
+                startupHelper.createHorizontalScroller(view, Sly);
+            }
+
+            focusManager.autoFocus(scrollSlider);
+        });
+    }
 
     return function (view, params) {
 
@@ -20,12 +135,12 @@ define(['loading', 'connectionManager', 'startup/startuphelper'], function (load
                 var apiClient = connectionManager.getApiClient(serverId);
                 apiClient.getPublicUsers().then(function (result) {
 
-                    startupHelper.renderLoginUsers(view, apiClient, result, serverId, !isRestored);
+                    renderLoginUsers(view, apiClient, result, serverId, !isRestored);
                     view.querySelector('.pageHeader').classList.remove('hide');
 
                 }, function (result) {
 
-                    startupHelper.renderLoginUsers(view, apiClient, [], serverId, !isRestored);
+                    renderLoginUsers(view, apiClient, [], serverId, !isRestored);
                 });
             });
 
--- startup/startuphelper.js
+++ startup/startuphelper.js
@@ -87,117 +87,6 @@ define(['connectionManager', 'loading', 'themeManager', 'focusManager'], functio
         }
     }
 
-    function renderLoginUsers(view, apiClient, users, serverId, initScroller) {
-
-        var items = users.map(function (user) {
-
-            var imgUrl = user.PrimaryImageTag ?
-                apiClient.getUserImageUrl(user.Id, {
-                    width: 400,
-                    tag: user.PrimaryImageTag,
-                    type: "Primary"
-                }) :
-                '';
-
-            var url = user.HasPassword ?
-                ('/startup/manuallogin.html?serverid=' + serverId + '&user=' + user.Name) :
-                '';
-
-            return {
-                name: user.Name,
-                showIcon: !imgUrl,
-                showImage: imgUrl,
-                icon: 'person',
-                cardImageStyle: "background-image:url('" + imgUrl + "');",
-                id: user.Id,
-                url: url,
-                serverId: user.ServerId,
-                defaultText: true
-            };
-
-        });
-
-        items.push({
-            name: Globalize.translate('core#ButtonManualLogin'),
-            showIcon: true,
-            showImage: false,
-            icon: 'lock',
-            cardImageStyle: '',
-            cardType: 'manuallogin',
-            defaultText: true,
-            url: '/startup/manuallogin.html?serverid=' + serverId
-        });
-
-        items.push({
-            name: Globalize.translate('core#EmbyConnect'),
-            showIcon: true,
-            showImage: false,
-            icon: 'cloud',
-            cardImageStyle: '',
-            cardType: 'embyconnect',
-            defaultText: true,
-            url: '/startup/connectlogin.html'
-        });
-
-        items.push({
-            name: Globalize.translate('core#ButtonChangeServer'),
-            showIcon: true,
-            showImage: false,
-            icon: 'cast',
-            cardImageStyle: '',
-            cardType: 'changeserver',
-            defaultText: true,
-            url: '/startup/selectserver.html'
-        });
-
-        var html = items.map(function (item) {
-
-            var secondaryText = item.defaultText ? '&nbsp;' : '';
-
-            var cardImageContainer;
-
-            if (item.showIcon) {
-                cardImageContainer = '<iron-icon class="cardImageIcon" icon="' + item.icon + '"></iron-icon>';
-            } else {
-                cardImageContainer = '<div class="cardImage" style="' + item.cardImageStyle + '"></div>';
-            }
-
-            var tagName = 'paper-button';
-            var innerOpening = '<div class="cardBox">';
-            var innerClosing = '</div>';
-
-            return '\
-<' + tagName + ' raised class="card squareCard loginSquareCard scalableCard" data-cardtype="' + item.cardType + '" data-url="' + item.url + '" data-name="' + item.name + '" data-serverid="' + item.serverId + '">\
-'+ innerOpening + '<div class="cardScalable">\
-<div class="cardPadder"></div>\
-<div class="cardContent">\
-<div class="cardImageContainer coveredImage defaultCardColor'+ getRandomInt(1, 5) + '">\
-'+ cardImageContainer + '</div>\
-</div>\
-</div>\
-<div class="cardFooter">\
-<div class="cardText">'+ item.name + '</div>\
-<div class="cardText dim">' + secondaryText + '</div>\
-</div>'+ innerClosing + '\
-</'+ tagName + '>';
-
-        }).join('');
-
-        var scrollSlider = view.querySelector('.scrollSlider');
-        scrollSlider.innerHTML = html;
-
-        require(["Sly"], function (Sly) {
-
-            loading.hide();
-
-            if (initScroller) {
-                createHorizontalScroller(view, Sly);
-            }
-
-            focusManager.autoFocus(scrollSlider);
-        });
-    }
-
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
@@ -388,8 +277,8 @@ define(['connectionManager', 'loading', 'themeManager', 'focusManager'], functio
         handleConnectionResult: handleConnectionResult,
         signIntoConnect: signIntoConnect,
         authenticateUser: authenticateUser,
-        renderLoginUsers: renderLoginUsers,
         onScrollSliderClick: onScrollSliderClick,
-        renderSelectServerItems: renderSelectServerItems
+        renderSelectServerItems: renderSelectServerItems,
+        createHorizontalScroller: createHorizontalScroller
     };
 });
