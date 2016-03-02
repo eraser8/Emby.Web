eb5e881606016bbef43a3785602478475c85f1c3
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-02 14:08:21 -0500

    update scrolling

--- components/sly/slyscroller.js
+++ components/sly/slyscroller.js
@@ -1,10 +1,38 @@
 define(['browser', 'Sly'], function (browser, Sly) {
 
+    function isSmartTv() {
+
+        // This is going to be really difficult to get right
+        var userAgent = navigator.userAgent.toLowerCase();
+
+        if (userAgent.indexOf('tv') != -1) {
+            return true;
+        }
+
+        if (userAgent.indexOf('samsung') != -1) {
+            return true;
+        }
+
+        if (userAgent.indexOf('nintendo') != -1) {
+            return true;
+        }
+
+        if (userAgent.indexOf('viera') != -1) {
+            return true;
+        }
+
+        if (userAgent.indexOf('xbox') != -1) {
+            return true;
+        }
+
+        return false;
+    }
+
     return {
         create: function (element, options) {
 
             if (options.enableAutoNativeScroll) {
-                if (browser.mobile) {
+                if (browser.mobile || isSmartTv()) {
 
                     options.enableNativeScroll = true;
                 } else {
